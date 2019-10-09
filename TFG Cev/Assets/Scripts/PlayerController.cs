1fadf0cfdc1199c4903316fcdf9c808ca7461669
Podgas <josupod@gmail.com>
2019-10-09 17:38:30 +0200

    Starting Movement Mechanics + Test Scene

---
+++ TFG Cev/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    Vector3 moveDirection;
+    float speed;
+    Transform myTransform;
+
+    void Start()
+    {
+        myTransform = transform;
+    }
+
+
+    void Update()
+    {
+
+
+        Move();
+
+        myTransform.Translate(moveDirection * speed * Time.deltaTime);
+
+    }
+
+    void Move()
+    {
+
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+
+}
