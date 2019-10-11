11999c61f2aab2ddc845482d1b1b0df08c31ada1
Podgas <josupod@gmail.com>
2019-10-12 00:28:35 +0200

    Going Forward with the Camera

---
+++ TFG Cev/Assets/Scripts/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float distanceAway;
+    [SerializeField]
+    private float distanceUp;
+    [SerializeField]
+    private float smooth;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f, 1.5f, 0f);
+    [SerializeField]
+    float rotateSpeed;
+
+    float horizontal;
+
+    private Vector3 lookDir;
+    private Vector3 targetPosition;
+
+    private Vector3 velocityCameraSmooth = Vector3.zero;
+    [SerializeField]
+    private float camSmoothDampTime = 0.1f;
+
+    void Start()
+    {
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+       
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+    }
+
+    void LateUpdate()
+    {
+
+        Vector3 characterOffset = target.position + offset;
+
+        lookDir = characterOffset - this.transform.position;
+        lookDir.y = 0;
+        lookDir.Normalize();
+        Debug.DrawRay(this.transform.position, lookDir, Color.green);
+
+        //Sets the target position for the camera with the offset
+        targetPosition = target.position + target.up * distanceUp - lookDir * distanceAway;
+
+        /*Debug.DrawRay(target.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(target.position, -1f* target.forward * distanceAway, Color.blue);
+        Debug.DrawLine(target.position, targetPosition);*/
+
+        //smoothing the current possition and the new position
+        SmoothPosition(this.transform.position, targetPosition);
+        
+
+        transform.LookAt(target);
+    }
+
+    private void SmoothPosition(Vector3 fromPos, Vector3 toPos)
+    {
+        this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCameraSmooth, camSmoothDampTime);
+    }
+}
+
--- TFG Cev/Assets/Scripts/PlayerController.cs
+++ TFG Cev/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    //------------Move Vars ---------------//
+    [Header("Movement")]
+    [SerializeField]
+    private float speed;
+    private float rotSpeed;
+    [SerializeField]
+    private float DirectionDampTime = .25f;
 
-    Vector3 moveDirection;
-    float speed;
+    private Vector3 _moveDirection;
+
     Transform myTransform;
+
+    Transform cameraPosition;
 
     void Start()
     {
         myTransform = transform;
+        cameraPosition = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
 
 
@@ -22,17 +32,42 @@ public class PlayerController : MonoBehaviour
 
         Move();
 
-        myTransform.Translate(moveDirection * speed * Time.deltaTime);
+       
+
+        myTransform.Translate(_moveDirection * speed * Time.deltaTime);
+
+        //StickToWorldSpace(transform, cameraPosition, ref _moveDirection);       
+
 
     }
+
+    //Moving the player 
 
     void Move()
     {
-
-        moveDirection.x = Input.GetAxisRaw("Horizontal");
-        moveDirection.y = Input.GetAxisRaw("Vertical");
-
+        _moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
     }
 
+
+    public void StickToWorldSpace(Transform root, Transform camera)
+    {
+        Vector3 rootDirection = root.forward;
+
+        Vector3 stickDirection = new Vector3(_moveDirection.x, 0, _moveDirection.z);
+
+        //get camera rotation
+        Vector3 cameraDirection = camera.forward;
+        cameraDirection.y = 0.0f;
+        Quaternion reference = Quaternion.FromToRotation(Vector3.forward, cameraDirection);
+
+        Vector3 moveDirection = reference * stickDirection;
+
+        Quaternion.LookRotation(moveDirection);
+
+
+
+        directionOut = moveDirection * angleRootToMove;
+
+    }
 
 }
