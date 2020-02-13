dec6acca9012815969527a30cc001d8fa6302818
Podgas <josupod@gmail.com>
2020-02-13 21:51:32 +0100

    Models added + Viewer and Level Setup

--- TFG Cev/Assets/Scenes/Others/IluminationScene/RotateInspector.cs
+++ TFG Cev/Assets/Scenes/Others/IluminationScene/RotateInspector.cs
@@ -10,26 +10,59 @@ public class RotateInspector : MonoBehaviour
     [SerializeField]
     GameObject[] models;
 
-    List<GameObject> gameObjectList;
+    [SerializeField]
+    List<Transform> gameObjectList;
 
+    int index = 0;
 
     private void Awake()
     {
-        if (models.Length >= 0)
+        
+    }
+    private void Start()
+    {
+        gameObjectList = new List<Transform>(transform.GetComponentsInChildren<Transform>());
+        List<Transform> removeObjects = new List<Transform>();
+        foreach (Transform t in gameObjectList)
         {
-            GameObject o = null;
-            foreach(GameObject model in models)
-            {
-                o = Instantiate(model, transform.position, Quaternion.identity);
-                gameObjectList.Add(o);
-                o.transform.parent = gameObject.transform;
-            }
+
+            if (t.parent == transform)
+                t.gameObject.SetActive(false);
+            
+            else
+                removeObjects.Add(t);
+            
 
         }
-    }
+        foreach (Transform t in removeObjects)
+        {
+            gameObjectList.Remove(t);
+        }
 
+        gameObjectList[0].gameObject.SetActive(true);
+        
+    }
     void Update()
     {
         transform.Rotate(0, speed * Time.deltaTime, 0);
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (index != 0)
+            {
+                gameObjectList[index].gameObject.SetActive(false);
+                index--;
+                gameObjectList[index].gameObject.SetActive(true);
+            }
+
+        }else if (Input.GetKeyDown(KeyCode.D))
+        {
+            if(index < gameObjectList.Count-1)
+            {
+                gameObjectList[index].gameObject.SetActive(false);
+                index++;
+                gameObjectList[index].gameObject.SetActive(true);
+            }
+        }
     }
 }
---
+++ TFG Cev/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    [SerializeField]
+    Animator faderAnim;
+
+    private SceneController.Scene levelToLoad;
+
+
+    public void FadeToLevel(SceneController.Scene scene)
+    {
+        levelToLoad = scene;
+        faderAnim.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneController.LoadScene(levelToLoad, false);
+    }
+
+    private void Update()
+    {
+        if(SceneController.GetActualScreenName() == SceneController.Scene.FinalScene.ToString())
+        {
+            if (Input.GetButton("Attack"))
+            {
+                FadeToLevel(SceneController.Scene.LogoScreen);
+            }
+        }
+    }
+}
