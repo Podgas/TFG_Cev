8335d1f17428358b2139bf4fb43c7bc1be32a1f9
Podgas <josupod@gmail.com>
2020-02-13 18:34:30 +0100

    Starting World building + Illumination

---
+++ TFG Cev/Assets/Scenes/Others/IluminationScene/RotateInspector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateInspector : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    GameObject[] models;
+
+    List<GameObject> gameObjectList;
+
+
+    private void Awake()
+    {
+        if (models.Length >= 0)
+        {
+            GameObject o = null;
+            foreach(GameObject model in models)
+            {
+                o = Instantiate(model, transform.position, Quaternion.identity);
+                gameObjectList.Add(o);
+                o.transform.parent = gameObject.transform;
+            }
+
+        }
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
