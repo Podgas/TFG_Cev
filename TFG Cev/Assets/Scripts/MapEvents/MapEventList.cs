cf5a8dbba03c0fc105c3bb4245f5f0edeb71008e
pdgames <josupod@gmail.com>
2020-05-18 15:57:55 +0200

    cualquier comentario, no se, lo que quieras -.-

---
+++ TFG Cev/Assets/Scripts/MapEvents/MapEventList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Game Event", menuName = "Map Event", order = 52)]
+
+public class MapEventList : ScriptableObject
+{
+    public Dictionary<string,bool> eventCheck;
+}
